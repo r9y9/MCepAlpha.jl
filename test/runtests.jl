ad4462946666b5ac50c6849000805e248ccacb66
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2014-11-08 13:37:29 +0900

    improve tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,13 @@
 using MCepAlpha
 using Base.Test
 
-@test_approx_eq  mcepalpha(8000)  0.312
-@test_approx_eq  mcepalpha(11025) 0.357
-@test_approx_eq  mcepalpha(16000) 0.41
-@test_approx_eq  mcepalpha(22050) 0.455
-@test_approx_eq  mcepalpha(44100) 0.544
-@test_approx_eq  mcepalpha(48000) 0.554
-
-@test_approx_eq  mcepalpha(8000.0)  0.312
+for f in (:uint, :int, :float)
+    @eval begin
+        @test_approx_eq  mcepalpha($f(8000))  0.312
+        @test_approx_eq  mcepalpha($f(11025)) 0.357
+        @test_approx_eq  mcepalpha($f(16000)) 0.41
+        @test_approx_eq  mcepalpha($f(22050)) 0.455
+        @test_approx_eq  mcepalpha($f(44100)) 0.544
+        @test_approx_eq  mcepalpha($f(48000)) 0.554
+    end
+end
